8146707214ebd8a0db0336bc23b01034027ca8ae
Celia <joyihirwecelia@gmail.com>
2026-09-25 22:26:02 +0200

    Plateform codebase

---
+++ Backend/models/User.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema(
+    {
+        fullName: {
+            type:String,
+            required:[true, "Full name is required"],
+            trim: true
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            minlength: 6
+        },
+        role: {
+            type: String,
+            enum: ["customer", "restaurant"],
+            required: true
+        },
+        isVerified: {
+            type: Boolean,
+            default: false
+        },
+    },
+    {
+        timestamps: true
+    }
+
+)
+const User = mongoose.model("User", userSchema)
+export default User
